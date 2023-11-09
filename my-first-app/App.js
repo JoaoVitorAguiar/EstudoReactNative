00afcb5d668171ae96697529c3636636f0352117
VITOR AGUIAR <joaovitor4guiar@gmail.com>
2023-11-09 19:41:45 -0300

    feat: add useState

--- my-first-app/App.js
+++ my-first-app/App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [saldo, setSaldo] = useState(100000);
   return (
     <View style={styles.container}>
-      <Image
-        source={{ uri: "https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-2-1.png" }}
-        style={{ width: 250, height: 150 }}
+      <Logo url="https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-2-1.png" />
+      <Text style={{ color: '#fff', fontSize: 20 }} >Olá Emanuel, seu saldo:</Text>
+      <Text style={{ color: '#fff', padding: 10, fontSize: 18 }}>R${saldo},00</Text>
+      <Button
+        onPress={() => setSaldo(saldo + 1000)}
+        title="Pix de R$ 1000,00"
       />
-      <Text style={{ color: '#fff' }} >Olá Cintia, seu saldo:</Text>
-      <Text style={{ color: '#fff' }}>R$ 100.000,00</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,3 +26,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
 });
+const Logo = props => {
+  return (
+
+    <Image
+      source={{ uri: props.url }}
+      style={{ width: 250, height: 150 }}
+    />
+  );
+};
+
